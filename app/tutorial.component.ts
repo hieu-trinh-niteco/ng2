77e7a620ea0ee340bec26d43ffb1f04f8bc5904a
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-22 17:52:38 +0700

    add first compnent

---
+++ app/tutorial.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-tutorial',
+    template: '<h2>This is my first Tutorial</h2>'
+})
+
+export class TutorialComponent{
+
+}
