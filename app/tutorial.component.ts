4fe174afc9c64803d64e14c31a407892cc1a4cae
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-23 17:51:20 +0700

    lesion11

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -4,13 +4,23 @@ import {Component} from '@angular/core'
     selector: 'my-tutorial',
     template: `
     <h2>{{ title }}</h2>
-    <input type="text" [(ngModel)]="fname" />
-    <input type="text" [(ngModel)]="lname" />
-    <br/>
-    Fullname: {{ fname }} {{ lname }}
+    <h3 *ngIf="showLineIf">This ngIf directive show</h3>
+    <div [ngSwitch]="color">
+        <p *ngSwitchCase="'red'">this is line red</p>
+        <p *ngSwitchCase="'blue'" >this is line blue</p>
+        <p *ngSwitchCase="'green'" >this is line green</p>
+        <p *ngSwitchDefault>Invalid color</p>
+    </div>
+
+    <ul>
+        <li *ngFor="let color of colors">{{ color }}</li>
+    </ul>
     `
 })
 
 export class TutorialComponent{
-    public title = "event binding tutorial"
+    public title = "directive tutorial"
+    public showLineIf = true
+    public color = "red"
+    public colors: string[] = ["red", "green", "blue"]
 }
