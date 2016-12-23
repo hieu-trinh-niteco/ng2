fca580dba6f381f063c4bbfca1dbb6603c06b7b7
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-23 14:50:51 +0700

    styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <h1>Hello {{name}}</h1>
+  <h4>angular component</h4>
   <my-tutorial></my-tutorial>`,
+  styles: ['h4 {color:blue}']
 })
 export class AppComponent  { name = 'Angular'; }
--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -2,7 +2,11 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'my-tutorial',
-    template: '<h2>This is my first Tutorial</h2>'
+    template: `
+    <h2>This is my first Tutorial</h2>
+    <h4>angular component</h4>
+    `,
+    styles: ['h2 {color : red;}','h4 {color:green}']
 })
 
 export class TutorialComponent{
