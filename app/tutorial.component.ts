41ba0d2b1ba7d4f6bdb0ad05de29b3a9f859c6ea
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-23 17:16:40 +0700

    lession 9

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -3,16 +3,16 @@ import {Component} from '@angular/core'
 @Component({
     selector: 'my-tutorial',
     template: `
-    <h2>This is my first Tutorial</h2>
-    <h3 [class.redColor]="applyClass">red class</h3>
-    <h4 [style.color]="blueColor ? 'blue' : 'orange'">inline style color</h4>
-    `,
-    styles: [`.redColor{
-        color:red
-    }`]
+    <h2>{{ title }}</h2>
+    <input type="text" #name />
+    <button (click)="OnClick($event)">Click Me</button>
+    `
 })
 
 export class TutorialComponent{
-    public applyClass = false;
-    public blueColor = true;
+    public title = "event binding tutorial"
+
+    OnClick(name:any){
+        console.log(name)
+    }
 }
