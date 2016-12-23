8819bdbfbb822294d3a2c44e59ccb2eedc813fe9
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-23 16:57:09 +0700

    lession 8

--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -2,9 +2,17 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'my-tutorial',
-    template: `<h2>This is my first Tutorial</h2>`,
+    template: `
+    <h2>This is my first Tutorial</h2>
+    <h3 [class.redColor]="applyClass">red class</h3>
+    <h4 [style.color]="blueColor ? 'blue' : 'orange'">inline style color</h4>
+    `,
+    styles: [`.redColor{
+        color:red
+    }`]
 })
 
 export class TutorialComponent{
-
+    public applyClass = false;
+    public blueColor = true;
 }
