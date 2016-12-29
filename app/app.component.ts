20ca84db12b27c80f053a14f0735aa548703a66f
Hieu Trinh <trieuhieu509a@gmail.com>
2016-12-29 16:17:12 +0700

    form template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
-import { TutorialComponent } from './tutorial.component';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>Hello {{ name | uppercase }}</h1>
-  <p>Date today: {{ today | date:'fullDate' | uppercase}}</p>
-  <p>Percenr: {{ number | percent: '4.3-5' }}</p>
-  <pre>{{ object | json }}</pre>
-  <ul>
-  <li *ngFor="let i of collection | slice:1:3">{{i}}</li>
-  </ul>
-  <p>2 power 10 = {{ 2 |  exponentialStrength:2}}</p>
-  <my-tutorial></my-tutorial>`,
+  templateUrl: 'app/app.component.html',
 })
 export class AppComponent  {
-  public name = 'AngularJs';
-  public today =Date.now();
-  public number = 1.34951234;
-  public object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
-  public collection: string[] = ['a', 'b', 'c', 'd'];
+  onSubmit(value:any){
+    console.log(value);
+  }
 }
